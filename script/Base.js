43d3f5a5b010242eaa0877e2c4dca34aba23154d
IStarterI <qkdwl462@gmail.com>
2018-05-22 16:07:30 +0900

    BiG Changed

--- script/Base.js
+++ script/Base.js
@@ -1,4 +1,8 @@
 window.onload = function() {
+  window.addEventListener("resize", function() {
+    $('.flipbook').turn('size', '60vw', '80vh');
+  });
+
   document.getElementById("Side").innerHTML =
     '<span id="Hambuger" class="icon fa fa-bars"></span> ' +
     '<a href="main.html"><span id="Home" class="icon fas fa-home"></span></a>' +
--- script/Main.js
+++ script/Main.js
@@ -1,13 +1,14 @@
 var i = 0;
-var introUrl = ['어서오세요! 당신을 기다렸습니다.', '이 곳은 한 고등학생의 이야기가 담긴 장소입니다.', '빈틈이 많은 만큼 더욱 친숙하고', '부족한 점이 많은 만큼 성장이 가능한', '바로 저의 이야기들이 담겨있습니다.', '사진들을 클릭하여 그 이야기들을 살펴보세요.']
+var introUrl = ['어서오세요! 당신을 기다렸습니다.', '이 곳은 한 고등학생의 이야기가 담긴 장소입니다.', '빈틈도 부족한 점도 많지만', '그만큼 성장 될 것이라 예상되는', '바로 저의 이야기들이 담겨있습니다.', '사진들을 클릭하여 제 이야기들을 살펴보세요.']
 var imgUrl = ['Me', 'School', 'Me', 'School', 'Me', 'School'];
+var HrefUrl = ['Me', 'One', 'Two', 'Three', 'Me', 'School'];
 
 document.getElementById("Prev").onclick = function() {
   if(i > 0) i -= 1;
   else i = introUrl.length - 1;
   document.getElementById("Intro").innerHTML = introUrl[i];
   document.getElementById("View").src = "./img/" + imgUrl[i] + ".jpg";
-  document.getElementById("ViewLink").href = "./" + imgUrl[i] + ".html";
+  document.getElementById("ViewLink").href = "./" + HrefUrl[i] + ".html";
 }
 
 document.getElementById("Next").onclick = function() {
