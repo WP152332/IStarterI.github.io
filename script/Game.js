9306559d6c36d21a3a698e95ec3ce0e8a4105d28
IStarterI <39394006+IStarterI@users.noreply.github.com>
2018-05-23 10:26:29 +0900

    Update Game.js

--- script/Game.js
+++ script/Game.js
@@ -19,29 +19,30 @@ for(var i = 0; i < 5; i++) {
 document.getElementById("Map" + CharacterX + "6").style.background = "Red";
 
 setInterval(function() {
-  var EneCnt = Math.floor(Math.random() * 5) + 1;
-  while(EneCnt == 6) EneCnt = Math.floor(Math.random() * 5) + 1;
-  var result = [];
   for(var x = 0; x < 5; x++) {
     for(var c = 5; c > 0; c--) {
      Map[x][c] = Map[x][c - 1]; 
      var color;
      switch(Map[x][c]) {
-      case 0: color = "none"; break;
-      case 1: color = "red"; break;
-      case 2: color = "yellow"; break;
-      case 3: color = "green"; break;
+       case 0:
+       case 1:
+       case 2: color = "none"; break;
+       case 3: color = "red"; break;
+       case 4: color = "yellow"; break;
+       case 5: color = "green"; break;
      }
     document.getElementById("Map" + x + c).style.background = color;
     }
-    Map[x][0] = Math.floor(Math.random() * 4);
-    while(Map[x][0] == 4) Map[x][0] = Math.floor(Math.random()*4);
+    Map[x][0] = Math.floor(Math.random() * 6);
+    while(Map[x][0] == 6) Map[x][0] = Math.floor(Math.random()*6);
     var color;
     switch(Map[x][0]) {
-      case 0: color = "none"; break;
-      case 1: color = "red"; break;
-      case 2: color = "yellow"; break;
-      case 3: color = "green"; break;
+       case 0:
+       case 1:
+       case 2: color = "none"; break;
+       case 3: color = "red"; break;
+       case 4: color = "yellow"; break;
+       case 5: color = "green"; break;
     }
     document.getElementById("Map" + x + 0).style.background = color;
   }
